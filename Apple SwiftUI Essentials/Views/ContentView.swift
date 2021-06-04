11027b59541a4b2a189e78682ebcdcb3f7012cf2
Kevin Tran <kevin2005tran@gmail.com>
2021-06-03 19:51:41 -0700

    created lists and nav

---
+++ Apple SwiftUI Essentials/Views/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  CircleImage.swift
+//  Apple SwiftUI Essentials
+//
+//  Created by Kevin Tran on 6/2/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
