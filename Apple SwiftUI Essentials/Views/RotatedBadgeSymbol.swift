42cae8c151571a447b463a7f141ca7ebe4a4eeaf
Kevin Tran <kevin2005tran@gmail.com>
2021-06-10 12:53:21 -0700

    added custom badge icon

---
+++ Apple SwiftUI Essentials/Views/RotatedBadgeSymbol.swift
@@ -0,0 +1,24 @@
+//
+//  RotatedBadgeSymbol.swift
+//  Apple SwiftUI Essentials
+//
+//  Created by Kevin Tran on 6/10/21.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+struct RotatedBadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        RotatedBadgeSymbol(angle: Angle(degrees: 5))
+    }
+}
